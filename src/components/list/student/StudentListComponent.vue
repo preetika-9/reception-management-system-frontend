8383a651efe7c0f6204b52e2932cf2ba5beeb335
pritika-01 <preetikapandey71@gmail.com>
2025-01-05 14:49:11 +0545

    student list

--- src/components/list/student/StudentListComponent.vue
+++ src/components/list/student/StudentListComponent.vue
@@ -1,17 +1,22 @@
 <template>
   <q-page class="q-pa-md">
-    <CustomTable :dynamic-store-name="storeName" :filter="filters"  :api-url="apiUrlPath" :route-path="routePath" :title="title"></CustomTable>
+    <CustomTable  :dynamic-store-name="storeName" :filter="filters"  :api-url="apiUrlPath" :route-path="routePath" :title="title">
+    </CustomTable>
   </q-page>
 </template>
 
 <script setup>
 import CustomTable from 'quasar-crud-yaj/src/templates/CustomTable.vue';
+import { createDynamicStore } from 'src/stores/dynamicCrudStore';
 
 
 const routePath = 'student';
 const apiUrlPath = 'student';
 const title = 'Student';
 const storeName = 'student';
+
+const dynamicStore = createDynamicStore(storeName, apiUrlPath);
+const store = dynamicStore();
 
 const props = defineProps({
   filters:{
